465d585aea5f9f5937b276f94d5472c6b524659f
chris abbott <chris.abs96@gmail.com>
2025-02-08 20:02:28 +0000

    feat: added new data migration for workspace relationships

--- internal/platform/database/init.go
+++ internal/platform/database/init.go
@@ -11,11 +11,11 @@ func (db *PostgresDB) Init() error {
         return fmt.Errorf("schema initialization failed: %v", err)
     }
 
-    // db.migrationsManager.EnableMigration("002_search_indexes")
-    // fmt.Println("Running data migration...")
-    // if err := db.migrationsManager.Run(); err != nil {
-    //     return fmt.Errorf("migrations failed: %v", err)
-    // }
+    db.migrationsManager.EnableMigration("003_workspace_relationships")
+    fmt.Println("Running data migration...")
+    if err := db.migrationsManager.Run(); err != nil {
+        return fmt.Errorf("migrations failed: %v", err)
+    }
 
     // UNCOMMENTING WILL DROP ALL TABLES IN DEV
     // if err := development.DropAllTables(db.DB); err != nil {
