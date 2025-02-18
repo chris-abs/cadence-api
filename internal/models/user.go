9d200f4001457381fe34c3b930c8014c63a7abb3
chris abbott <chris.abs96@gmail.com>
2025-02-18 23:06:16 +0000

    chore: rethinking family management approach

--- internal/models/user.go
+++ internal/models/user.go
@@ -2,6 +2,13 @@ package models
 
 import "time"
 
+type UserRole string
+
+const (
+    RoleParent UserRole = "PARENT"
+    RoleChild  UserRole = "CHILD"
+)
+
 type User struct {
     ID         int         `json:"id"`
     Email      string      `json:"email"`
@@ -9,16 +16,9 @@ type User struct {
     FirstName  string      `json:"firstName"`
     LastName   string      `json:"lastName"`
     ImageURL   string      `json:"imageUrl"`
-    Role       UserRole    `json:"role"`        
-    FamilyID   *int        `json:"familyId"`    
+    Role       UserRole    `json:"role"`
+    FamilyID   *int        `json:"familyId"`
     Containers []Container `json:"containers"`
     CreatedAt  time.Time   `json:"createdAt"`
     UpdatedAt  time.Time   `json:"updatedAt"`
 }
-
-type UserRole string
-
-const (
-    RoleParent UserRole = "PARENT"
-    RoleChild  UserRole = "CHILD"
-)
