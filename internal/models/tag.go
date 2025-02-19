706bb07c3e99939ef9fc4484d35614cdef06aeeb
chris abbott <chris.abs96@gmail.com>
2025-02-19 22:53:53 +0000

    feat: added family id to entity models

--- internal/models/tag.go
+++ internal/models/tag.go
@@ -7,6 +7,7 @@ type Tag struct {
     Name        string    `json:"name"`
     Colour      string    `json:"colour"`
     Description string    `json:"description"`
+    FamilyID    int         `json:"familyId"` 
     Items       []Item    `json:"items"`
     CreatedAt   time.Time `json:"createdAt"`
     UpdatedAt   time.Time `json:"updatedAt"`
