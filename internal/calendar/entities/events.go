80e2f29af5724f22971f96e6d6faede7ea9877d6
chris abbott <chris.abs96@gmail.com>
2025-04-30 22:46:10 +0100

    feat: scaffolding structs

---
+++ internal/calendar/entities/events.go
@@ -0,0 +1,24 @@
+package entities
+
+import "time"
+
+type Event struct {
+    ID          int       `json:"id"`
+    Title       string    `json:"title"`
+    Description string    `json:"description"`
+    Start       time.Time `json:"start"`
+    End         time.Time `json:"end"`
+    AllDay      bool      `json:"allDay"`
+    
+    CreatedBy   int       `json:"createdBy"`  
+    AssigneeIDs []int     `json:"assigneeIds"`
+    Color       *string   `json:"color,omitempty"`
+    
+    Type        string    `json:"type"`
+    ModuleID    *string   `json:"moduleId,omitempty"`
+    EntityID    *int      `json:"entityId,omitempty"`
+    
+    FamilyID    int       `json:"familyId"`
+    CreatedAt   time.Time `json:"createdAt"`
+    UpdatedAt   time.Time `json:"updatedAt"`
+}
---
+++ internal/calendar/model.go
@@ -0,0 +1,31 @@
+package calendar
+
+import "time"
+
+type CreateEventRequest struct {
+    Title       string    `json:"title"`
+    Description string    `json:"description"`
+    Start       time.Time `json:"start"`
+    End         time.Time `json:"end"`
+    AllDay      bool      `json:"allDay"`
+    AssigneeIDs []int     `json:"assigneeIds"`
+    Color       *string   `json:"color,omitempty"`
+}
+
+type UpdateEventRequest struct {
+    Title       string    `json:"title"`
+    Description string    `json:"description"`
+    Start       time.Time `json:"start"`
+    End         time.Time `json:"end"`
+    AllDay      bool      `json:"allDay"`
+    AssigneeIDs []int     `json:"assigneeIds"`
+    Color       *string   `json:"color,omitempty"`
+}
+
+type GetEventsParams struct {
+    Start       time.Time `json:"start"`
+    End         time.Time `json:"end"`
+    AssigneeIDs []int     `json:"assigneeIds,omitempty"`
+    Types       []string  `json:"types,omitempty"`
+    ModuleIDs   []string  `json:"moduleIds,omitempty"`
+}
