1311774cf8e73dfa10c3047f70417b24ae06d438
chris abbott <chris.abs96@gmail.com>
2025-05-06 19:45:28 +0100

    feat: get events with parameters - WIP

--- internal/calendar/model.go
+++ internal/calendar/model.go
@@ -2,28 +2,30 @@ package calendar
 
 import "time"
 
+type GetEventsParams struct {
+    StartTime    time.Time `schema:"startTime"`
+    EndTime      time.Time `schema:"endTime"`
+    AssigneeID   *int     `schema:"assigneeId,omitempty"`
+    ModuleIDs    []string `schema:"moduleIds,omitempty"`
+    SourceID     *int     `schema:"sourceId,omitempty"`
+}
+
 type CreateEventRequest struct {
-    Title        string    `json:"title" validate:"required"`
-    Description  *string   `json:"description"`
-    Location     *string   `json:"location"`
-    StartTime    time.Time `json:"startTime" validate:"required"`
-    EndTime      time.Time `json:"endTime" validate:"required,gtfield=StartTime"`
-    AllDay       bool      `json:"allDay"`
-    AssigneeID   int       `json:"assigneeId" validate:"required"`
+    Title       string    `json:"title" validate:"required"`
+    Description *string   `json:"description"`
+    Location    *string   `json:"location"`
+    StartTime   time.Time `json:"startTime" validate:"required"`
+    EndTime     time.Time `json:"endTime" validate:"required,gtfield=StartTime"`
+    AllDay      bool      `json:"allDay"`
+    AssigneeID  int       `json:"assigneeId" validate:"required"`
 }
 
 type UpdateEventRequest struct {
-    Title        string    `json:"title" validate:"required"`
-    Description  *string   `json:"description"`
-    Location     *string   `json:"location"`
-    StartTime    time.Time `json:"startTime" validate:"required"`
-    EndTime      time.Time `json:"endTime" validate:"required,gtfield=StartTime"`
-    AllDay       bool      `json:"allDay"`
-    AssigneeID   int       `json:"assigneeId" validate:"required"`
-}
-
-type GetEventsParams struct {
-    StartTime    time.Time `schema:"startTime" validate:"required"`
-    EndTime      time.Time `schema:"endTime" validate:"required,gtfield=StartTime"`
-    AssigneeID   *int     `schema:"assigneeId"`
+    Title       string    `json:"title" validate:"required"`
+    Description *string   `json:"description"`
+    Location    *string   `json:"location"`
+    StartTime   time.Time `json:"startTime" validate:"required"`
+    EndTime     time.Time `json:"endTime" validate:"required,gtfield=StartTime"`
+    AllDay      bool      `json:"allDay"`
+    AssigneeID  int       `json:"assigneeId" validate:"required"`
 }
