ccc7a2139f6cdb34ae0537487bbe334832f2e2d9
chris abbott <chris.abs96@gmail.com>
2025-05-13 23:46:52 +0100

    chore: misc

--- internal/calendar/entities/events.go
+++ internal/calendar/entities/events.go
@@ -12,22 +12,22 @@ const (
 )
 
 type Event struct {
-    ID           int       `json:"id"`
-    Title        string    `json:"title"`
-    Description  *string   `json:"description,omitempty"`
-    Location     *string   `json:"location,omitempty"`
-    StartTime    time.Time `json:"startTime"`
-    EndTime      time.Time `json:"endTime"`
-    AllDay       bool      `json:"allDay"`
-    CreatedBy    int       `json:"createdBy"`
-    AssigneeID   int       `json:"assigneeId"`
-    Type         string    `json:"type"`
-    SourceModule string    `json:"sourceModule"`
-    SourceID     *int      `json:"sourceId,omitempty"`
-    FamilyID     int       `json:"familyId"`
-    CreatedAt    time.Time `json:"createdAt"`
-    UpdatedAt    time.Time `json:"updatedAt"`
-    IsDeleted    bool      `json:"isDeleted"`
+    ID           int        `json:"id"`
+    Title        string     `json:"title"`
+    Description  *string    `json:"description,omitempty"`
+    Location     *string    `json:"location,omitempty"`
+    StartTime    time.Time  `json:"startTime"`
+    EndTime      time.Time  `json:"endTime"`
+    AllDay       bool       `json:"allDay"`
+    CreatedBy    int        `json:"createdBy"`
+    AssigneeID   int        `json:"assigneeId"`
+    Type         string     `json:"type"`
+    SourceModule string     `json:"sourceModule"`
+    SourceID     *int       `json:"sourceId,omitempty"`
+    FamilyID     int        `json:"familyId"`
+    CreatedAt    time.Time  `json:"createdAt"`
+    UpdatedAt    time.Time  `json:"updatedAt"`
+    IsDeleted    bool       `json:"isDeleted"`
     DeletedAt    *time.Time `json:"deletedAt,omitempty"`
-    DeletedBy    *int      `json:"deletedBy,omitempty"`
+    DeletedBy    *int       `json:"deletedBy,omitempty"`
 }
