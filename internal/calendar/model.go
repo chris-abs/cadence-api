208b85eb4d7b773118157ed6e6f3aa31d5d3a39f
chris abbott <chris.abs96@gmail.com>
2025-05-17 23:51:50 +0100

    feat: adjusting models to include recurrsion

--- internal/calendar/model.go
+++ internal/calendar/model.go
@@ -1,6 +1,10 @@
 package calendar
 
-import "time"
+import (
+	"time"
+
+	"github.com/chrisabs/cadence/internal/calendar/entities"
+)
 
 type GetEventsParams struct {
     StartTime    time.Time `schema:"startTime"`
@@ -8,16 +12,20 @@ type GetEventsParams struct {
     AssigneeIDs  []int     `schema:"assigneeIds,omitempty"` 
     ModuleIDs    []string  `schema:"moduleIds,omitempty"`
     SourceID     *int      `schema:"sourceId,omitempty"`
+    Limit        int       `schema:"limit,omitempty"`
+    Offset       int       `schema:"offset,omitempty"`
 }
 
 type CreateEventRequest struct {
-    Title       string    `json:"title" validate:"required"`
-    Description *string   `json:"description"`
-    Location    *string   `json:"location"`
-    StartTime   time.Time `json:"startTime" validate:"required"`
-    EndTime     time.Time `json:"endTime" validate:"required,gtfield=StartTime"`
-    AllDay      bool      `json:"allDay"`
-    AssigneeID  int       `json:"assigneeId" validate:"required"`
+    Title       string     `json:"title" validate:"required"`
+    Description *string    `json:"description"`
+    Location    *string    `json:"location"`
+    StartTime   time.Time  `json:"startTime" validate:"required"`
+    EndTime     time.Time  `json:"endTime" validate:"required,gtfield=StartTime"`
+    AllDay      bool       `json:"allDay"`
+    AssigneeID  int        `json:"assigneeId" validate:"required"`
+    RepeatType  string     `json:"repeatType,omitempty"` 
+    RepeatUntil *time.Time `json:"repeatUntil,omitempty"`
 }
 
 type UpdateEventRequest struct {
@@ -28,4 +36,10 @@ type UpdateEventRequest struct {
     EndTime     time.Time `json:"endTime" validate:"required,gtfield=StartTime"`
     AllDay      bool      `json:"allDay"`
     AssigneeID  int       `json:"assigneeId" validate:"required"`
+    UpdatedBy   int       `json:"updatedBy" validate:"required"`
+}
+
+type EventResponse struct {
+    *entities.Event
+    HasMore bool `json:"hasMore,omitempty"`
 }
