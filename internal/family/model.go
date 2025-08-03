f7d20712b0134746726bc60330503485c253db7f
chris abbott <chris.abs96@gmail.com>
2025-08-03 17:33:06 +0100

    chore: id rework family

--- internal/family/model.go
+++ internal/family/model.go
@@ -7,7 +7,7 @@ import (
 )
 
 type FamilyAccount struct {
-    ID          int                 `json:"id"`
+    ID          models.FamilyID     `json:"id"`
     Email       string              `json:"email"`
     Password    string              `json:"-"` 
     FamilyName  string              `json:"familyName"`
@@ -18,7 +18,7 @@ type FamilyAccount struct {
 }
 
 type FamilySettings struct {
-	FamilyID  int                 `json:"familyId"`
+	FamilyID  models.FamilyID     `json:"familyId"`
 	Modules   []models.Module     `json:"modules"`
 	Status    models.FamilyStatus `json:"status"`
 	CreatedAt time.Time           `json:"createdAt"`
--- internal/models/types.go
+++ internal/models/types.go
@@ -2,16 +2,16 @@ package models
 
 import "github.com/google/uuid"
 
-type FamilyID    string
-type ProfileID   string
+type FamilyID string
+type ProfileID string
 
-type MediaID     string
-type ChoreID     string
-type EventID     string
+type MediaID string
+type ChoreID string
+type EventID string
 type ContainerID string
-type ItemID      string
+type ItemID string
 type WorkspaceID string
-type TagID       string
+type TagID string
 
 func NewFamilyID() FamilyID {
     return FamilyID(uuid.New().String())
@@ -19,4 +19,50 @@ func NewFamilyID() FamilyID {
 
 func NewProfileID() ProfileID {
     return ProfileID(uuid.New().String())
+}
+
+func NewMediaID() MediaID {
+    return MediaID(uuid.New().String())
+}
+
+func NewChoreID() ChoreID {
+    return ChoreID(uuid.New().String())
+}
+
+func NewEventID() EventID {
+    return EventID(uuid.New().String())
+}
+
+func NewContainerID() ContainerID {
+    return ContainerID(uuid.New().String())
+}
+
+func NewItemID() ItemID {
+    return ItemID(uuid.New().String())
+}
+
+func NewWorkspaceID() WorkspaceID {
+    return WorkspaceID(uuid.New().String())
+}
+
+func NewTagID() TagID {
+    return TagID(uuid.New().String())
+}
+
+func (id FamilyID) IsValid() bool {
+    _, err := uuid.Parse(string(id))
+    return err == nil
+}
+
+func (id ProfileID) IsValid() bool {
+    _, err := uuid.Parse(string(id))
+    return err == nil
+}
+
+func (id FamilyID) String() string {
+    return string(id)
+}
+
+func (id ProfileID) String() string {
+    return string(id)
 }
