18487ed324b77b156dfd5679cbe719a955d641fc
chris abbott <chris.abs96@gmail.com>
2025-08-07 00:09:08 +0100

    chore: id rework storage recent

--- internal/storage/recent/service.go
+++ internal/storage/recent/service.go
@@ -1,5 +1,7 @@
 package recent
 
+import "github.com/chrisabs/cadence/internal/models"
+
 type Service struct {
     repo *Repository
 }
@@ -8,7 +10,7 @@ func NewService(repo *Repository) *Service {
     return &Service{repo: repo}
 }
 
-func (s *Service) GetRecentEntities(familyID int) (*Response, error) {
+func (s *Service) GetRecentEntities(familyID models.FamilyID) (*Response, error) {
     const defaultLimit = 10
     return s.repo.GetRecentEntities(familyID, defaultLimit)
 }
