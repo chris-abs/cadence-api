04d0035f05b3b4dae1ecdce67d42d15808928788
chris abbott <chris.abs96@gmail.com>
2025-08-07 00:00:26 +0100

    chore: id rework storage tag

--- internal/storage/tag/service.go
+++ internal/storage/tag/service.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/chrisabs/cadence/internal/models"
 	"github.com/chrisabs/cadence/internal/storage/entities"
 )
 
@@ -15,7 +16,7 @@ func NewService(repo *Repository) *Service {
     return &Service{repo: repo}
 }
 
-func (s *Service) CreateTag(familyID int, profileID int, req *CreateTagRequest) (*entities.Tag, error) {
+func (s *Service) CreateTag(familyID models.FamilyID, profileID models.ProfileID, req *CreateTagRequest) (*entities.Tag, error) {
     tag := &entities.Tag{
         Name:        req.Name,
         Description: req.Description,
@@ -34,15 +35,15 @@ func (s *Service) CreateTag(familyID int, profileID int, req *CreateTagRequest) 
     return s.repo.GetByID(tag.ID, familyID)
 }
 
-func (s *Service) GetTagByID(id int, familyID int) (*entities.Tag, error) {
+func (s *Service) GetTagByID(id models.TagID, familyID models.FamilyID) (*entities.Tag, error) {
     return s.repo.GetByID(id, familyID)
 }
 
-func (s *Service) GetAllTags(familyID int) ([]*entities.Tag, error) {
+func (s *Service) GetAllTags(familyID models.FamilyID) ([]*entities.Tag, error) {
     return s.repo.GetByFamilyID(familyID)
 }
 
-func (s *Service) UpdateTag(id int, familyID int, profileID int, req *UpdateTagRequest) (*entities.Tag, error) {
+func (s *Service) UpdateTag(id models.TagID, familyID models.FamilyID, profileID models.ProfileID, req *UpdateTagRequest) (*entities.Tag, error) {
     tag, err := s.repo.GetByID(id, familyID)
     if err != nil {
         return nil, fmt.Errorf("tag not found: %v", err)
@@ -51,7 +52,7 @@ func (s *Service) UpdateTag(id int, familyID int, profileID int, req *UpdateTagR
     tag.Name = req.Name
     tag.Description = req.Description
     tag.Colour = req.Colour
-    tag.ProfileID = profileID 
+    tag.ProfileID = profileID
     tag.UpdatedAt = time.Now().UTC()
 
     if err := s.repo.Update(tag); err != nil {
@@ -61,15 +62,15 @@ func (s *Service) UpdateTag(id int, familyID int, profileID int, req *UpdateTagR
     return s.repo.GetByID(id, familyID)
 }
 
-func (s *Service) AssignTagsToItems(familyID int, tagIDs []int, itemIDs []int) error {
+func (s *Service) AssignTagsToItems(familyID models.FamilyID, tagIDs []models.TagID, itemIDs []models.ItemID) error {
     return s.repo.AssignTagsToItems(familyID, tagIDs, itemIDs)
 }
 
-func (s *Service) DeleteTag(id int, familyID int, deletedBy int) error {
+func (s *Service) DeleteTag(id models.TagID, familyID models.FamilyID, deletedBy models.ProfileID) error {
     return s.repo.Delete(id, familyID, deletedBy)
 }
 
-func (s *Service) RestoreTag(id int, familyID int) error {
+func (s *Service) RestoreTag(id models.TagID, familyID models.FamilyID) error {
     if err := s.repo.RestoreDeleted(id, familyID); err != nil {
         return fmt.Errorf("failed to restore tag: %v", err)
     }
